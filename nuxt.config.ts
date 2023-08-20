550b12e23b8245751975b05c0bd77e837a0abbd5
malt9830 <malt9830@stud.kea.dk>
2023-08-20 16:44:22 +0200

    Made font work again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,6 @@ export default {
   meta: {
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,500;0,600;0,700;1,500;1,600;1,700&display=swap",
-      },
     ],
   },
   css: ["@/assets/css/main.css"],
@@ -21,7 +17,7 @@ export default {
   },
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", ["@nuxtjs/google-fonts", {
     families: {
-      Inter: [500, 600, 700]
+      'Montserrat Alternates': [500, 600, 700]
     },
   }]],
 };

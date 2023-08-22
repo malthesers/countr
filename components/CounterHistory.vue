1fbec6525f7ec0f9ab23e7f5563cc53df670f780
malt9830 <malt9830@stud.kea.dk>
2023-08-22 02:12:24 +0200

    Added transition to history

--- components/CounterHistory.vue
+++ components/CounterHistory.vue
@@ -1,7 +1,9 @@
 <template>
   <footer class="absolute w-full bottom-0 bg-violet-900 p-4 font-mono text-yellow-100">
     <div class="text-xl flex gap-[12px] overflow-x-scroll">
-      <span v-for="number in historyStore.history" :key="number">{{ number }}</span>
+      <TransitionGroup name="slide">
+        <span v-for="number in historyStore.history" :key="number">{{ number }}</span>
+      </TransitionGroup>
     </div>
     <div class="absolute top-0 left-0 w-full h-full z-10 bg-gradient-to-r from-transparent to-violet-900 pointer-events-none"/>
   </footer>
@@ -31,5 +33,14 @@ counterStore.$subscribe(() => {
 
 ::-webkit-scrollbar-thumb {
   background: #4c1d95;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 0.5s ease;
+}
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
 }
 </style>

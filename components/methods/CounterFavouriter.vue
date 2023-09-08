1a3797a047309e113668aae98aa094a2413766ae
malt9830 <malt9830@stud.kea.dk>
2023-09-08 02:59:18 +0200

    Added 'clicked' transition to reset/fav buttons

--- components/methods/CounterFavouriter.vue
+++ components/methods/CounterFavouriter.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="addFavourite" class="h-[68px] w-full mt-4 text-3xl border-2 border-yellow-100 rounded-full duration-200 hover:bg-yellow-100 hover:text-violet-800">favourite</button>
+  <button @click="addFavourite" class="h-[68px] w-full mt-4 text-3xl border-2 border-yellow-100 rounded-full duration-200 hover:bg-yellow-100 hover:text-violet-800 active:scale-90">favourite</button>
 </template>
 
 <script setup>
--- components/methods/CounterResetter.vue
+++ components/methods/CounterResetter.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="counterStore.$reset" class="h-[68px] w-full mt-4 text-3xl border-2 border-yellow-100 rounded-full duration-200 hover:bg-yellow-100 hover:text-violet-800">reset</button>
+  <button @click="counterStore.$reset" class="h-[68px] w-full mt-4 text-3xl border-2 border-yellow-100 rounded-full duration-200 hover:bg-yellow-100 hover:text-violet-800 active:scale-90">reset</button>
 </template>
 
 <script setup>

619c43ea4527e744c26e0890ffada7040314e733
malt9830 <malt9830@stud.kea.dk>
2023-09-08 03:17:01 +0200

    Added clicking transitions for multiplier and replacer

--- components/methods/CounterMultiplier.vue
+++ components/methods/CounterMultiplier.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-[68px] w-full mt-4 text-3xl rounded-full grid grid-cols-3">
+  <div class="h-[68px] w-full mt-4 text-3xl rounded-full grid grid-cols-3 duration-200 active:scale-90">
     <button @click="changeMethod(1)" :class="{ 'bg-yellow-100 text-violet-800' : counterStore.method === 1}" class="border-2 border-yellow-100 rounded-l-full duration-200 hover:bg-yellow-100 hover:text-violet-800">x1</button>
     <button @click="changeMethod(2)" :class="{ 'bg-yellow-100 text-violet-800' : counterStore.method === 2}" class="border-y-2 border-yellow-100 duration-200 hover:bg-yellow-100 hover:text-violet-800">x2</button>
     <button @click="changeMethod(3)" :class="{ 'bg-yellow-100 text-violet-800' : counterStore.method === 3}" class="border-2 border-yellow-100 rounded-r-full duration-200 hover:bg-yellow-100 hover:text-violet-800">x3</button>
